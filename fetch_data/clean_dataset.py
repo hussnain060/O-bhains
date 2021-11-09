1ddcb85bb91478190d57d24e481f19e7e1fb25a6
Hussnain Ali <58597950+hussnain060@users.noreply.github.com>
2021-11-09 21:34:34 +0500

    Update clean_dataset.py

--- fetch_data/clean_dataset.py
+++ fetch_data/clean_dataset.py
@@ -16,33 +16,6 @@ sheet1_df = sheet1_df.iloc[:, :6]
 sheet1_df = sheet1_df.dropna()
 
 
-# Average Temperature and movement of cow per day
-
-date_df = sheet1_df["date"].drop_duplicates()
-days_temperature_avg = []
-days_X_axis_avg = []
-days_Y_axis_avg = []
-days_Z_axis_avg = []
-for x in date_df:
-    days_data = sheet1_df.loc[sheet1_df["date"] == x]
-    days_temperature_avg.append(
-        days_data["temperature"].sum() / len(days_data["temperature"])
-    )
-    days_X_axis_avg.append(days_data["x_axix"].sum() / len(days_data["x_axix"]))
-    days_Y_axis_avg.append(days_data["y_axix"].sum() / len(days_data["y_axix"]))
-    days_Z_axis_avg.append(days_data["z_axix"].sum() / len(days_data["z_axix"]))
-
-df = pd.DataFrame(
-    {
-        "Date": date_df,
-        "Temperature_avg": days_temperature_avg,
-        "X_axis_avg": days_X_axis_avg,
-        "Y_axis_avg": days_Y_axis_avg,
-        "Z_axis_avg": days_Z_axis_avg,
-    }
-)
-df = df.round(2)
-
-# Final dataset
-print(df)
-df.to_csv("./data/from_fetch_data.csv", index=False)
+# Dataset
+print(sheet1_df)
+sheet1_df.to_csv("./data/from_fetch_data.csv", index=False)
