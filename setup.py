a0a1a7c0db2a58e1178e441681360fbfa04752f2
fahad_linux <fahadakbar@gmail.com>
2021-11-20 10:48:43 -0500

    initializing pytest

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["pandas", "numpy", "gspread", "gspread-dataframe",],
+    install_requires=["pandas", "numpy", "gspread", "gspread-dataframe", "pytest"],
     keywords=["python", "video", "stream", "video stream", "camera stream", "sockets"],
     classifiers=[
         "Development Status :: 1 - Planning",
